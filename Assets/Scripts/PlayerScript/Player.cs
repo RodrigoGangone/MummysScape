412c7c64c87edd0f6c8210f82fafba9620df8cc3
Aurelio007 <aurelio.ayala@davinci.edu.ar>
2024-04-13 12:10:00 -0300

    Player
    player

--- Assets/Scripts/PlayerScript/Player.cs
+++ Assets/Scripts/PlayerScript/Player.cs
@@ -7,6 +7,22 @@ public class Player : MonoBehaviour
     ModelPlayer _modelPlayer;
     ViewPlayer _viewPlayer;
     ControllerPlayer _controllerPlayer;
+    StateMachinePlayer _machinePlayer;
+
+    private float _life;
+    private float _speed;
+    private float _stockBandages;
 
 
+
+    public float Life { get => _life; set => _life = value; }
+    public float Speed { get => _speed; set { } }
+
+    private void Start()
+    {
+        _modelPlayer = new ModelPlayer();
+        _controllerPlayer = new ControllerPlayer();
+        _machinePlayer = new StateMachinePlayer();
+        
+    }
 }
