e05740cb88c56be0f1339af94c8e189579dd619d
Aurelio007 <aurelio.ayala@davinci.edu.ar>
2024-04-13 11:48:55 -0300

    Player
    player

---
+++ Assets/Scripts/PlayerScript/Player.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    ModelPlayer _modelPlayer;
+    ViewPlayer _viewPlayer;
+    ControllerPlayer _controllerPlayer;
+
+
+}
