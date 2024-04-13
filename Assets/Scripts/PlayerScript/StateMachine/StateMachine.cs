2aa054b0162b2d64a1e529988f087d117dcb9665
MaximilianoBarbieri <129629883+MaximilianoBarbieri@users.noreply.github.com>
2024-04-13 12:11:02 -0300

    Creacion de estados
    -Se crearon estados de stateMachine
    -Se creo script para stateMachine

---
+++ Assets/Scripts/PlayerScript/StateMachine/StateMachine.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    Dictionary<Enum, State> _allStates = new();
+    State _currentState;
+
+    public void Update()
+    {
+        _currentState?.OnUpdate();
+    }
+
+    public void FixedUpdate()
+    {
+        _currentState?.OnFixedUpdate();
+    }
+
+    public void AddState(Enum name, State state)
+    {
+        if (!_allStates.ContainsKey(name))
+        {
+            _allStates.Add(name, state);
+            state.StateMachine = this;
+        }
+        else
+        {
+            _allStates[name] = state;
+        }
+    }
+
+    public String getCurrentState()
+    {
+        return (_currentState != null) ? _currentState.ToString() : "No hay estado";
+    }
+
+    public void ChangeState(Enum name)
+    {
+        if (_allStates.ContainsKey(name) && !_allStates[name].Equals(_currentState))
+        {
+            _currentState?.OnExit();
+            if (_allStates.ContainsKey(name)) _currentState = _allStates[name];
+            _currentState?.OnEnter();   
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScript/StateMachine/States/SM_Grab.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SM_Grab : State
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnEnter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnFixedUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnExit()
+    {
+        throw new System.NotImplementedException();
+    }
+}
