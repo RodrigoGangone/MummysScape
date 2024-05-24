a1e225f6c0a6f6117dfdecfa9c8a206d3474782d
RodrigoGangoneDavinci <rodrigo.gangone@davinci.edu.ar>
2024-05-24 01:14:14 -0300

    code

--- Assets/Scripts/Platforms/MovePlatform.cs
+++ Assets/Scripts/Platforms/MovePlatform.cs
@@ -1,17 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MovePlatform : MonoBehaviour
 {
+    [Header("MOVE")]
     [SerializeField] private float speed = 1.0f;
-    [SerializeField] private float moveX = 0f;
-    [SerializeField] private float moveY = 0f;
-    [SerializeField] private float moveZ = 0f;
+    [SerializeField] private float moveX;
+    [SerializeField] private float moveY;
+    [SerializeField] private float moveZ;
+    
+    [Header("ROTATE")]
+    [SerializeField] private bool rotateYAxi;
 
     private Vector3 _startPosition;
     private float _time;
-    
+
     void Start()
     {
         _startPosition = transform.position;
@@ -26,5 +28,10 @@ public class MovePlatform : MonoBehaviour
         float z = moveZ != 0 ? Mathf.Sin(_time) * moveZ : 0f;
 
         transform.position = _startPosition + new Vector3(x, y, z);
+    }
+
+    private void Rotate()
+    {
+        transform.Rotate(0f,90f,0f);
     }
 }
