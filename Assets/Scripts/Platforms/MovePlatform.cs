64a2af9733288be153ef78cea35dd00bd66b1780
RodrigoGangoneDavinci <rodrigo.gangone@davinci.edu.ar>
2024-05-24 02:05:25 -0300

    cambios en el moviemiento de plataformas

--- Assets/Scripts/Platforms/MovePlatform.cs
+++ Assets/Scripts/Platforms/MovePlatform.cs
@@ -1,37 +1,49 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class MovePlatform : MonoBehaviour
 {
-    [Header("MOVE")]
-    [SerializeField] private float speed = 1.0f;
-    [SerializeField] private float moveX;
+    [Header("SPEED")] [SerializeField] private float speed = 1.0f;
+
+    [Header("MOVE")] [SerializeField] private float moveX;
     [SerializeField] private float moveY;
     [SerializeField] private float moveZ;
-    
-    [Header("ROTATE")]
-    [SerializeField] private bool rotateYAxi;
 
-    private Vector3 _startPosition;
+    [Header("ROTATE")] [SerializeField] private float rotateY;
+
+    [Header("START POSITION")] [SerializeField]
+    private Transform centerOfSin;
+
     private float _time;
 
     void Start()
     {
-        _startPosition = transform.position;
+        if (centerOfSin != null)
+        {
+            //metodo para ir hasta el centerposition
+            transform.position = centerOfSin.position;
+        }
     }
 
     void Update()
     {
         _time += Time.deltaTime * speed;
 
+        //Move
         float x = moveX != 0 ? Mathf.Sin(_time) * moveX : 0f;
         float y = moveY != 0 ? Mathf.Sin(_time) * moveY : 0f;
         float z = moveZ != 0 ? Mathf.Sin(_time) * moveZ : 0f;
 
-        transform.position = _startPosition + new Vector3(x, y, z);
+        //Rotation
+        float ry = rotateY != 0 ? rotateY * 0.01f * _time : 0f;
+
+        //transform.position = new Vector3(x, y, z);
+        transform.position.Set(x, y, z);
+        transform.Rotate(0, ry, 0);
     }
 
-    private void Rotate()
+    private void ActivatePlatform()
     {
-        transform.Rotate(0f,90f,0f);
+        transform.Rotate(0f, 90f, 0f);
     }
 }
