227c441afb81e97d076184a363a2f1de1c4cd1db
RodrigoGangoneDavinci <rodrigo.gangone@davinci.edu.ar>
2024-05-24 00:38:15 -0300

    move platform

--- Assets/Scripts/Platforms/MovePlatform.cs
+++ Assets/Scripts/Platforms/MovePlatform.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class MovePlatform : MonoBehaviour
 {
-    [SerializeField] private
+    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float moveX = 0f;
+    [SerializeField] private float moveY = 0f;
+    [SerializeField] private float moveZ = 0f;
+
+    private Vector3 _startPosition;
+    private float _time;
     
-    // Start is called before the first frame update
     void Start()
     {
-        
+        _startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _time += Time.deltaTime * speed;
+
+        float x = Mathf.Sin(_time) * moveX;
+        float y = Mathf.Sin(_time) * moveY;
+        float z = Mathf.Sin(_time) * moveZ;
+
+        transform.position = _startPosition + new Vector3(x, y, z);
     }
 }
