6ea1df0eff0de86eda7a3295cad3ce31ac5f62aa
MaximilianoBarbieri <129629883+MaximilianoBarbieri@users.noreply.github.com>
2024-05-25 00:18:37 -0300

    Fix

--- Assets/Scripts/DetectionBeetle.cs
+++ Assets/Scripts/DetectionBeetle.cs
@@ -10,7 +10,8 @@ public class DetectionBeetle : MonoBehaviour
     public Rigidbody currentBeetle;
 
     private Transform _beetleFX;
-    
+
+
     private void OnTriggerEnter(Collider other) // Agregar el Beetle con el que colisiono el trigger a la lista
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Beetle"))
@@ -21,12 +22,11 @@ public class DetectionBeetle : MonoBehaviour
 
     private void OnTriggerStay(Collider other) //Recorrer una lista de collider para saber cual es el mas cercano
     {
-        if(other.gameObject.layer != LayerMask.NameToLayer("Beetle")) return;
-        
-        float nearestDistance = float.MaxValue;
-        
+        if (other.gameObject.layer != LayerMask.NameToLayer("Beetle") || _beetles.Count == 0) return;
         _beetleFX = other.transform.GetChild(0);
-        
+
+        float nearestDistance = float.MaxValue;
+
         if (_beetles.Count > 1)
         {
             foreach (Collider beetle in _beetles)
@@ -38,7 +38,7 @@ public class DetectionBeetle : MonoBehaviour
                     nearestDistance = distance;
 
                     currentBeetle = beetle.GetComponent<Rigidbody>();
-                   
+
                     _beetleFX.gameObject.SetActive(true);
                 }
                 else
@@ -52,23 +52,22 @@ public class DetectionBeetle : MonoBehaviour
             currentBeetle = _beetles[0].GetComponent<Rigidbody>();
             _beetleFX.gameObject.SetActive(true);
         }
-        else
-        {
-            currentBeetle = null;
-            return;
-        }
     }
 
     private void OnTriggerExit(Collider other) // Eliminar el Beetle que agregue
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Beetle"))
+        if (other.gameObject.layer != LayerMask.NameToLayer("Beetle")) return;
+
+        _beetleFX = other.transform.GetChild(0);
+        _beetleFX.gameObject.SetActive(false);
+
+        var cur = other.GetComponent<Rigidbody>();
+
+        _beetles.Remove(other);
+
+        if (currentBeetle == cur)
         {
-            _beetleFX = other.transform.GetChild(0);
-            _beetleFX.gameObject.SetActive(false);
-
-            if (currentBeetle == other) currentBeetle = null;
-
-            _beetles.Remove(other);
+            currentBeetle = null;
         }
     }
 }
