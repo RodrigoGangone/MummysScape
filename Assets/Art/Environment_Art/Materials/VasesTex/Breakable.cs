e67a1af20978ca5434aece564f2e85618e20eb69
MaximilianoBarbieri <129629883+MaximilianoBarbieri@users.noreply.github.com>
2024-06-28 00:02:49 -0300

    Drop de vendas

--- Assets/Art/Environment_Art/Materials/VasesTex/Breakable.cs
+++ Assets/Art/Environment_Art/Materials/VasesTex/Breakable.cs
@@ -6,12 +6,18 @@ using UnityEngine;
 public class Breakable : MonoBehaviour
 {
     [SerializeField] private GameObject _destroyeVersion;
+    [SerializeField] private GameObject _drop;
+    [SerializeField] private bool _droped;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Bullet") || other.gameObject.CompareTag("PlayerFather"))
-        { 
+        {
             Instantiate(_destroyeVersion, transform.position, transform.rotation);
+
+            if (_droped)
+                Instantiate(_drop, transform.position, transform.rotation);
+
             Destroy(gameObject);
         }
     }
