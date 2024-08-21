63123ef3702b244a76d14137291ff65783428e06
MaximilianoBarbieri <129629883+MaximilianoBarbieri@users.noreply.github.com>
2024-08-21 00:30:43 -0300

    Correccion ref Hook
    -Se corrigio la perdida de referencia del Hook cuando te movias antes de que la venda llegue a destino.
    -Ahora, el currentHook se define dentro del estado [Hook] para evitar la "Ventana" donde perdia la referencia.

--- Assets/Scripts/PlayerScript/StateMachine/States/SM_Hook.cs
+++ Assets/Scripts/PlayerScript/StateMachine/States/SM_Hook.cs
@@ -18,8 +18,8 @@ public class SM_Hook : State
 
     public override void OnEnter()
     {
-        Debug.Log("ON ENTER HOOK");
-
+        _model.hookBeetle = _model.detectionBeetle.currentHook;
+        
         _view.StateRigBuilder(true);
         _view.PLAY_ANIM("Hook", true);
         _view.bandageHook.enabled = true;
@@ -83,6 +83,8 @@ public class SM_Hook : State
         _isHookDestiny = false;
 
         _model.isHooking = false;
+
+        _model.hookBeetle = null;
 
         Object.Destroy(_model.springJoint);
     }
