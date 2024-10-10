71e87f17689d416fe0c0cf7fbc2ca51223684352
lihuelhurtado <129628632+lihuelhurtado@users.noreply.github.com>
2024-10-10 19:07:19 -0300

    Cajas, cambio de camara lvl4

---
+++ Assets/Scripts/Interactuable/Visual/BoxFX.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxFX : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem sandMoundParticle;
+    [SerializeField] private GameObject sandMound;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Sand"))
+        {
+            Debug.Log("Sand");
+            if (!sandMoundParticle.isPlaying)
+            {
+                sandMoundParticle.Play();
+            }
+        }
+    }
+}
