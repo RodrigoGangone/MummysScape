d62ec986bb21ea367df624707d6f3d0b5c94c52f
RodrigoGangoneDavinci <rodrigo.gangone@davinci.edu.ar>
2024-11-01 00:46:08 -0300

    mejora de nodos en formato prefab

--- Assets/Scripts/Camera/CameraNode.cs
+++ Assets/Scripts/Camera/CameraNode.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,6 +6,27 @@ public class CameraNode : MonoBehaviour
 {
     [Tooltip("Nodos a los que este nodo puede conectarse.")]
     public List<CameraNode> connectedNodes;  // Lista de nodos con los que puede conectar
+    public Vector3 Position { get; private set; }
+    
+    private CameraPathManager cameraPathManager;
 
-    public Vector3 Position => transform.position;  // La posición del nodo en la escena
+    private void Start()
+    {
+        //La posicion del primer hije
+        Position = transform.GetChild(0).position;
+        
+        cameraPathManager = Camera.main.GetComponent<CameraPathManager>();
+        if (cameraPathManager == null)
+        {
+            Debug.LogError("CameraPathManager no encontrado en la cámara principal.");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PlayerFather") && cameraPathManager != null && this != null)
+        {
+            cameraPathManager.MoveCameraToNode(this);
+        }
+    }
 }
