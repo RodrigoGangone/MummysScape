f337ab57fb70b23e9a93140f3da20d0096275634
RodrigoGangoneDavinci <rodrigo.gangone@davinci.edu.ar>
2024-11-13 18:44:05 -0300

    Audio Manager and sound script

---
+++ Assets/Scripts/Sounds/AudioManager.cs
@@ -0,0 +1,52 @@
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+    
+    public Sound[] musicSounds, sfxSounds;
+    public AudioSource musicSource, sfxSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        PlayMusic("Theme");
+    }
+
+    public void PlayMusic(string name)
+    {
+        Sound s = Array.Find(musicSounds, x => x.name == name);
+
+        if (s != null)
+        {
+            musicSource.clip = s.clip;
+            musicSource.Play();
+        }
+        else Debug.Log("Music Not Found");
+    }
+
+    public void PlaySFX(string name)
+    {
+        Sound s = Array.Find(sfxSounds, x => x.name == name);
+
+        if (s != null)
+        {
+            sfxSource.clip = s.clip;
+            sfxSource.Play();
+        }
+        else Debug.Log("Sfx Not Found");
+    }
+}
