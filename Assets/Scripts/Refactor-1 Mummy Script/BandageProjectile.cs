b13c75186ca8545dd7e35a06fea6eeccf49a2cfc
MaximilianoBarbieri <129629883+MaximilianoBarbieri@users.noreply.github.com>
2025-10-14 20:39:02 -0300

    fix-Mejoras en Proyectil

--- Assets/Scripts/Refactor-1 Mummy Script/BandageProjectile.cs
+++ Assets/Scripts/Refactor-1 Mummy Script/BandageProjectile.cs
@@ -2,21 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class BandageProjectile : MonoBehaviour
 {
-    public void Play(IReadOnlyList<Vector3> path, float speed) => StartCoroutine(Run(path, speed));
-    
-    private IEnumerator Run(IReadOnlyList<Vector3> path, float speed)
+    private Rigidbody _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.useGravity = false;
+    }
+
+    public void Play(IReadOnlyList<Vector3> path, float speed) => StartCoroutine(RunPhysics(path, speed));
+    private IEnumerator RunPhysics(IReadOnlyList<Vector3> path, float speed)
     {
         foreach (var target in path)
         {
-            while (transform.position != target)
+            while (Vector3.Distance(_rb.position, target) > 0.1f)
             {
-                transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
-                transform.LookAt(target);
-                yield return null;
+                var newPosition = Vector3.MoveTowards(_rb.position, target, speed * Time.fixedDeltaTime);
+                _rb.MovePosition(newPosition);
+                
+                var dir = target - _rb.position;
+                if (dir.sqrMagnitude > 0.001f)
+                {
+                    var targetRot = Quaternion.LookRotation(dir.normalized);
+                    var newRot = Quaternion.Slerp(_rb.rotation, targetRot, Time.fixedDeltaTime);
+                    _rb.MoveRotation(newRot);
+                }
+                yield return new WaitForFixedUpdate();
             }
         }
+        
+        _rb.useGravity = true;
     }
 }
 
