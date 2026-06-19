af22fd9e7e8348cfebc6b83ae50a4f3b054ffbb2
MaximilianoBarbieri <129629883+MaximilianoBarbieri@users.noreply.github.com>
2026-06-18 23:36:37 -0300

    update - Fuego que se mueve

---
+++ Assets/Scripts/Actors/Mummy/View/MummyColorSetSO.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewMummyColorSet", menuName = "Mummy's Colors/Color Set")]
+public class MummyColorSetSO : ScriptableObject
+{
+    //[Header("Craneo (Standard Material)")]
+    //public Color craneoAlbedo = Color.white;
+
+    [Header("Shader Fuego 1 (Standard Colors)")]
+    
+    public Color fire1Bottom = Color.red;
+    public Color fire1Mid = Color.yellow;
+    public Color fire1Top = Color.white;
+
+    [Header("Shader Fuego 2 (HDR Colors)")]
+    
+    [ColorUsage(true, true)] public Color fire2Bottom = Color.red;
+    [ColorUsage(true, true)] public Color fire2Mid = Color.yellow;
+    [ColorUsage(true, true)] public Color fire2Top = Color.white;
+    [ColorUsage(true, true)] public Color fire2Glow = Color.magenta;
+}
